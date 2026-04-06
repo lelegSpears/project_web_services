8bbf469a904862097f7739630db673cf1d6ad259
lelegSpears <lelegsoares@gmail.com>
2026-04-06 19:01:07 -0300

    feat:Initial Commit

---
+++ src/main/java/com/lelegspears/project_wev_services/ProjectWevServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lelegspears.project_wev_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWevServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWevServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lelegspears/project_wev_services/ProjectWevServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lelegspears.project_wev_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWevServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
