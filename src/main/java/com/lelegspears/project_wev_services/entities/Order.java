24bdee617d55eebb44455fcd150e1a8041006fc5
lelegSpears <lelegsoares@gmail.com>
2026-04-09 00:36:28 -0300

    feat:Implementando OrderStatus

--- src/main/java/com/lelegspears/project_wev_services/entities/Order.java
+++ src/main/java/com/lelegspears/project_wev_services/entities/Order.java
@@ -1,7 +1,9 @@
 package com.lelegspears.project_wev_services.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.lelegspears.project_wev_services.entities.enums.OrderStatus;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,13 +30,23 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @Setter(AccessLevel.NONE)
+    private Integer orderStatus;
+
     Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus  orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
---
+++ src/main/java/com/lelegspears/project_wev_services/entities/enums/OrderStatus.java
@@ -0,0 +1,27 @@
+package com.lelegspears.project_wev_services.entities.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum OrderStatus {
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+
+    private int code;
+
+    OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            if (orderStatus.getCode() == code) {
+                return orderStatus;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Order Status");
+    }
+}
