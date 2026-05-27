ce87c9fdeb5644a3208328aeacb04a27c4eb0fab
lelegSpears <lelegsoares@gmail.com>
2026-05-27 15:56:40 -0300

    test:  Tstes básicos de service e controller

---
+++ src/test/java/com/lelegspears/project_wev_services/user/UserControllerTest.java
@@ -0,0 +1,102 @@
+package com.lelegspears.project_wev_services.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lelegspears.project_wev_services.exception.handler.ResourceExceptionHandler;
+import com.lelegspears.project_wev_services.exception.service.ResourceNotFoundException;
+import com.lelegspears.project_wev_services.user.controller.UserController;
+import com.lelegspears.project_wev_services.user.dtos.UserResponseDTO;
+import com.lelegspears.project_wev_services.user.entity.User;
+import com.lelegspears.project_wev_services.user.service.UserService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.context.annotation.Import;
+
+import org.springframework.http.MediaType;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
+@Import(ResourceExceptionHandler.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService service;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Long id;
+
+    private User user;
+
+    private UserResponseDTO userResponseDTO;
+
+    @BeforeEach
+    void setUp() {
+        // arrange
+        id = 1L;
+
+        user = new User(
+                id,
+                "Leandro",
+                "leandro@email.com",
+                "11999999999",
+                "123456"
+        );
+
+        userResponseDTO  = new UserResponseDTO();
+
+        userResponseDTO.setId(id);
+        userResponseDTO.setName("Leandro");
+        userResponseDTO.setEmail("leandro@email.com");
+
+    }
+
+    @Test
+    void findById_ShouldReturnUserDTO_WhenUserExists() throws Exception {
+
+        given(service.findById(id)).willReturn(userResponseDTO);
+
+        // act + assert
+        mockMvc.perform(get("/users/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value("Leandro"))
+                .andExpect(jsonPath("$.email").value("leandro@email.com"));
+
+        verify(service).findById(id);
+        verifyNoMoreInteractions(service);
+    }
+
+    @Test
+    void findById_ShouldReturnNotFound_WhenUserDoesNotExist() throws Exception {
+
+        Long id = 99L;
+
+        given(service.findById(id))
+                .willThrow(new ResourceNotFoundException(id));
+
+        mockMvc.perform(get("/users/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+
+        verify(service).findById(id);
+        verifyNoMoreInteractions(service);
+    }
+}           // Focar estudos em Spring Security e depois retornar para aprofundar testes
---
+++ src/test/java/com/lelegspears/project_wev_services/user/UserServiceTest.java
@@ -0,0 +1,106 @@
+package com.lelegspears.project_wev_services.user;
+
+import com.lelegspears.project_wev_services.exception.service.ResourceNotFoundException;
+import com.lelegspears.project_wev_services.user.dtos.UserResponseDTO;
+import com.lelegspears.project_wev_services.user.entity.User;
+import com.lelegspears.project_wev_services.user.mapper.UserMapper;
+import com.lelegspears.project_wev_services.user.repository.UserRepository;
+import com.lelegspears.project_wev_services.user.service.UserService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService service;
+
+    private Long id;
+
+    private User user;
+
+    private UserResponseDTO userResponseDTO;
+
+    @BeforeEach
+    void setUp() {
+        id = 1L;
+
+        user = new User(
+                id,
+                "Leandro",
+                "leandro@email.com",
+                "11999999999",
+                "123456"
+        );
+
+        userResponseDTO  = new UserResponseDTO();
+
+        userResponseDTO.setId(id);
+        userResponseDTO.setName("Leandro");
+        userResponseDTO.setEmail("leandro@email.com");
+
+    }
+
+    @Test
+    void findById_ShouldReturnUserDTO_WhenUserExists() {
+
+        given(repository.findById(id)).willReturn(Optional.of(user));
+        given(mapper.toDTO(user)).willReturn(userResponseDTO);
+
+        UserResponseDTO result = service.findById(id);
+
+        assertNotNull(result);
+        assertEquals(id, result.getId());
+        assertEquals("Leandro", result.getName());
+        assertEquals("leandro@email.com", result.getEmail());
+
+        verify(repository).findById(id);
+        verify(mapper).toDTO(user);
+
+        verifyNoMoreInteractions(repository, mapper);
+    }
+
+    @Test
+    void findById_ShouldThrowResourceNotFoundException_WhenUserDoesNotExist() {
+
+
+        Long nonExistId = 99L;
+
+        given(repository.findById(nonExistId)).willReturn(Optional.empty());
+
+
+        ResourceNotFoundException ex = assertThrows(
+                ResourceNotFoundException.class,
+                () -> service.findById(nonExistId)
+        );
+
+        assertEquals("User not found", ex.getMessage());
+
+        verify(repository).findById(nonExistId);
+
+        verifyNoInteractions(mapper);
+        verifyNoMoreInteractions(repository);
+    }
+}
