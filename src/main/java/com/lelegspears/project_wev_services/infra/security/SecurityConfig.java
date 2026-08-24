e401be9a9c839fabbd76c653363d34ab3ede67b6
lelegSpears <lelegsoares@gmail.com>
2026-08-24 03:14:17 -0300

    feat: Liberando o h2 para test.yml

--- src/main/java/com/lelegspears/project_wev_services/infra/security/SecurityConfig.java
+++ src/main/java/com/lelegspears/project_wev_services/infra/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/users").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
